900a691638fb71edcff684033e54c5799bac38bc
DiegoSB9 <diego_sb_95@hotmail.com>
2024-11-25 18:21:37 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,64 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("Variables actuales: ", variables);
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
+  // Portada
+  let cover = variables.includeCover
+    ? `<div class="cover"><img src="${variables.background ||
+        "https://via.placeholder.com/800x200"}" /></div>`
+    : `<div class="cover"></div>`;
+
+  // Nombre completo
+  let fullName = `${variables.name || "Name"} ${variables.lastName ||
+    "Last Name"}`;
+
+  // Obtenemos la posición desde las variables o del select si no está definida
+  let socialMediaPositionClass =
+    variables.socialMediaPosition ||
+    document.querySelector('[for="socialMediaPosition"]').value;
+
+  // Íconos de redes sociales
+  let socialMediaLinks = `
+        ${
+          variables.twitter
+            ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+            : ""
+        }
+        ${
+          variables.github
+            ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+            : ""
+        }
+        ${
+          variables.linkedin
+            ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+            : ""
+        }
+        ${
+          variables.instagram
+            ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+            : ""
+        }
+      `;
+
+  document.querySelector("#widget_content").innerHTML = `
+      <div class="widget">
+        ${cover}
+        <img src="${variables.avatarURL ||
+          "https://via.placeholder.com/150"}" class="photo" />
+        <h1>${fullName}</h1>
+        <h2>${variables.role || "Role"}</h2>
+        <h3>${variables.city || "City"}, ${variables.country || "Country"}</h3>
+        <ul class="position-${socialMediaPositionClass}">
+          ${socialMediaLinks}
+        </ul>
+      </div>
     `;
+
+  document.querySelector(
+    '[for="socialMediaPosition"]'
+  ).value = socialMediaPositionClass;
 }
 
 /**
